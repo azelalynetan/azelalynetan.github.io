aad554431003206bcd88cb1e6a95b09764500c1a
azelalynetan <azelalynetan@gmail.com>
2025-08-30 23:43:17 +0800

    Update description

--- app/components/home/jumbotron/Description.vue
+++ app/components/home/jumbotron/Description.vue
@@ -7,8 +7,12 @@
       </div>
       
       <p class="mb-2">
-        I’m a frontend developer based on the Philippines 🇵🇭 with a focus on building modern, accessible, and performant websites. 
-        I enjoy working with Vue, Nuxt, and Tailwind to bring designs to life and create experiences that feel intuitive.
+        I&apos;m a frontend developer based in the Philippines 🇵🇭, 
+        passionate about creating modern and accessible websites. 
+        I mainly work with Nuxt and Tailwind to build smooth, user-friendly experiences. 
+        I&apos;m always learning—Next.js is next on my list 
+        and I also have some experience working with backend tools 
+        like APIs and Supabase to better understand how everything connects.
       </p>
       <p class="mb-2">
         Beyond coding, I believe the small details matter — from smooth interactions to layouts that just feel right. I’ve also been an active volunteer in tech communities, where I loved sharing knowledge and connecting with fellow developers.
