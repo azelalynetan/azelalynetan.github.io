0618cc5dcbdb59efe5e4e9712e5e035392df47b5
azelalynetan <azelalynetan@gmail.com>
2025-09-06 00:40:21 +0800

    Update some minor changes to navbar, card and activity page description

--- app/components/activities/CardList.vue
+++ app/components/activities/CardList.vue
@@ -6,13 +6,18 @@
     <div v-else class="flex flex-col gap-4">
       <div v-for="activity in activities" class="border-brand-darkPink bg-brand-lightPink border-2 rounded-md shadow-lg">
         <div class="p-10">
+          <div class="text-xs">
+            <span>{{activity.date}}</span>
+          </div>
           <NuxtLink :to="activity.path" class="font-bold text-base block mb-1">
             {{activity.title}}
           </NuxtLink>
           <div class="flex flex-col sm:flex-row sm:gap-2 text-sm">
-            <span>{{activity.date}}</span>
-            <span class="hidden sm:block">•</span>
-            <span>{{activity.description}}</span>
+            <!-- <span class="hidden sm:block">•</span> -->
+            <!-- <span>{{activity.description}}</span> -->
+            <div v-if="activity.tags && activity.tags.length > 0" class="flex gap-2">
+              <UiBadge v-for="tag in activity.tags" variant="gray" :key="tag">{{ tag }}</UiBadge>
+            </div>
           </div>
         </div>
         <!-- <div v-if="activity.image" class="h-full max-w-60 object-cover aspect-[1.91/1] hidden sm:block">
@@ -27,13 +32,13 @@
 const { data: activities, status } = await useAsyncData('activities', async () => {
   const items = await queryCollection('activities')
     .order('date', 'DESC')
-    .select('title', 'date', 'description', 'image', "path")
+    .select('title', 'date', 'description', 'image', "path", "tags")
     .all()
 
   return items.map((item) => ({
     title: item.title,
     description: item.description,
-    tags: item.meta?.tags,
+    tags: item.tags,
     date: item.date
       ? new Date(item.date).toLocaleDateString('en-US', {
           year: 'numeric',
